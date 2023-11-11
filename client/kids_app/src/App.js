6bfcc295e71658d0cff63850a75d2e6c9bb6b23c
ParsaKargari <parsa.kargari@ucalgary.ca>
2023-11-11 14:28:29 -0700

    Add axios package and implement user data upload feature with Cloudinary integration

--- client/kids_app/src/App.js
+++ client/kids_app/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [info, setInfo] = useState('');
+
+    const handleImageChange = (e) => {
+        setSelectedImage(e.target.files[0]);
+    };
+
+    const handleUpload = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('image', selectedImage);
+        formData.append('email', email);
+        formData.append('name', name);
+        formData.append('phone', phone);
+        formData.append('info', info);
+
+        try {
+            const response = await axios.post('http://localhost:8628/add_user', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            console.log('Response:', response.data);
+            alert('Data uploaded successfully');
+        } catch (error) {
+            console.error('Error during data upload:', error);
+            alert('Error during data upload');
+        }
+    };
+
+    return (
+        <div className="App">
+            <h1>User Data Upload</h1>
+            <form onSubmit={handleUpload}>
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} /><br />
+                <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} /><br />
+                <input type="tel" placeholder="Phone" value={phone} onChange={(e) => setPhone(e.target.value)} /><br />
+                <textarea placeholder="Info" value={info} onChange={(e) => setInfo(e.target.value)} /><br />
+                <input type="file" onChange={handleImageChange} /><br />
+                <button type="submit">Upload Data</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
--- server/server.py
+++ server/server.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import boto3
 from botocore.exceptions import ClientError
+import cloudinary
+import cloudinary.uploader
+# Random UUID generator
+import uuid
 
 app = Flask(__name__)
 CORS(app)
@@ -15,26 +19,34 @@ dynamodb = boto3.resource(
 )
 table_name = 'UserTable'
 
+cloudinary.config(
+    cloud_name="dlb4j1jyd",
+    api_key="911761227391725",
+    api_secret="GD-qbQ3hupMF5Sh1peNGy8JZXlY"
+)
+
 # Create Table in DynamoDB
+
+
 def create_table():
     try:
         table = dynamodb.create_table(
             TableName=table_name,
             KeySchema=[
                 {
-                    'AttributeName': 'user_id',
+                    'AttributeName': 'UUID',
                     'KeyType': 'HASH'  # Partition key
                 }
             ],
             AttributeDefinitions=[
                 {
-                    'AttributeName': 'user_id',
+                    'AttributeName': 'UUID',
                     'AttributeType': 'S'
                 }
             ],
             ProvisionedThroughput={
-                'ReadCapacityUnits': 10,
-                'WriteCapacityUnits': 10
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
             }
         )
         table.wait_until_exists()  # Wait for table creation
@@ -45,26 +57,74 @@ def create_table():
 # Uncomment the below line to create the table. Run once.
 # create_table()
 
-# Add Test Data
-@app.route('/add_test_data', methods=['POST'])
-def add_test_data():
-    table = dynamodb.Table(table_name)
-    test_items = [
-        {'user_id': '1', 'username': 'UserOne', 'info': 'Some info about User One'},
-        {'user_id': '2', 'username': 'UserTwo', 'info': 'Some info about User Two'}
-    ]
+# Upload image to Cloudinary
 
-    for item in test_items:
-        try:
-            table.put_item(Item=item)
-        except ClientError as e:
-            return jsonify({"error": str(e)}), 500
 
-    return jsonify({"message": "Test data added successfully"}), 201
+def upload_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image found"}), 400
 
-# Other routes here...
+    image = request.files['image']
+
+    if image.filename == '':
+        return jsonify({"error": "No image found"}), 400
+
+    try:
+        upload_result = cloudinary.uploader.upload(image)
+        image_url = upload_result.get('url')
+
+        return jsonify({"image_url": image_url}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# Helper function to upload image to Cloudinary
+
+
+def upload_image_to_cloudinary(image_file):
+    try:
+        upload_result = cloudinary.uploader.upload(image_file)
+        return upload_result.get('url')
+    except Exception as e:
+        return None
+
+
+# Add user to DynamoDB table
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    # Extracting text fields from request.form, not request.json
+    email = request.form.get('email')
+    name = request.form.get('name')
+    phone = request.form.get('phone')
+    info = request.form.get('info')
+    image_file = request.files.get('image')
+
+    # Upload image and get URL
+    image_url = upload_image_to_cloudinary(image_file) if image_file else None
+
+    # Generate UUID
+    user_uuid = str(uuid.uuid4())
+
+    # Construct user data
+    user_data = {
+        'UUID': user_uuid,
+        'Email': email,
+        'Name': name,
+        'Phone': phone,
+        'Info': info,
+        'ImageURL': image_url
+    }
+
+    # Add user to DynamoDB table
+    try:
+        table = dynamodb.Table(table_name)
+        table.put_item(Item=user_data)
+        return jsonify({"message": "User added successfully", "UUID": user_uuid}), 200
+    except ClientError as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     # Run the app on all interfaces on port 8628
-    create_table()
+    # Uncomment the below line to create the table. Run once.
+    # create_table()
     app.run(debug=True, host='0.0.0.0', port=8628)
