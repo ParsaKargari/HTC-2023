24716702a2634e336c1648f7fdaf7e434b7f873a
mahirazabin <mahirazabin@gmail.com>
2023-11-11 16:40:06 -0700

    Donate Form

--- client/kids_app/src/pages/Donate/index.jsx
+++ client/kids_app/src/pages/Donate/index.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./style.css";
 
 const Donate = () => {
-  return <div>index 3</div>;
+  return (
+    <form>
+      <h1>Enter Item Details <span>Below</span></h1>
+      <input type="text" title="title" id="" placeholder="Enter Title"/>
+      <input type="size" title="size" id="" placeholder="Size (S/M/L)"/> 
+      <textarea name="description" id="" cols="30" rows="10" placeholder="Description (Optional)"/>
+      <button type='upload'>Upload Image</button>
+      <button type='submit'>send</button>
+    </form>
+  )
 };
 
 export default Donate;
