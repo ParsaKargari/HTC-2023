71c63dde85e0535193d22637ccd7685fd0ac76cb
Debo Dam <debojeetdam.business@gmail.com>
2023-11-11 15:09:02 -0700

    word

--- client/kids_app/src/pages/Landing/index.jsx
+++ client/kids_app/src/pages/Landing/index.jsx
@@ -3,10 +3,12 @@ import "./style.css";
 
 const Landing = () => {
   return (
-    <div className="hero section__padding" id="home">
-      <div className="hero-content">
-        <h1>TinyThreads</h1>
-        <p>DONATE NOW | BUY NOW</p>
+    <div className="hero">
+      <div className="section__padding" id="home">
+        <div className="hero-content">
+          <h1>TinyThreads</h1>
+          <p>DONATE NOW | BUY NOW</p>
+        </div>
       </div>
     </div>
   );
