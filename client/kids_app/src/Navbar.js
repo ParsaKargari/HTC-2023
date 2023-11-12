c63a203e107e232a1dc587bc9076aedaa38b42f4
Debo Dam <debojeetdam.business@gmail.com>
2023-11-11 20:54:06 -0700

    hi

--- client/kids_app/src/Navbar.js
+++ client/kids_app/src/Navbar.js
@@ -1,22 +1,41 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+// Navbar.js
+
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import logo from "../../kids_app/src/assets/logo";
+import "../src/components/";
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My Application
-        </Typography>
-        <Button color="inherit" component={Link} to="/landing">Home</Button>
-        <Button color="inherit" component={Link} to="/registration">Registration</Button>
-        <Button color="inherit" component={Link} to="/profile">Profile</Button>
-        <Button color="inherit" component={Link} to="/donate">Donate</Button>
-        <Button color="inherit" component={Link} to="/buynow">Buy Now</Button>
+    <AppBar position="static" className="navbar">
+      <Toolbar className="navbar-links">
+        <div className="navbar-links_logo">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="navbar-links_container">
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            My Application
+          </Typography>
+          <Button color="inherit" component={Link} to="/landing">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/registration">
+            Registration
+          </Button>
+          <Button color="inherit" component={Link} to="/profile">
+            Profile
+          </Button>
+          <Button color="inherit" component={Link} to="/donate">
+            Donate
+          </Button>
+          <Button color="inherit" component={Link} to="/buynow">
+            Buy Now
+          </Button>
+        </div>
       </Toolbar>
     </AppBar>
   );
--- client/kids_app/src/pages/Buynow/index.jsx
+++ client/kids_app/src/pages/Buynow/index.jsx
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 import "./style.css";
 
 const Buynow = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "http://10.12.170.44:8628/get_listings_t2"
+        );
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 5000);
+
+    return () => clearInterval(intervalId); // Clear interval on component unmount
+  }, []);
+
   const createCards = () => {
-    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    return numbers.map((val) => {
-      return <h1>{val}</h1>;
-    });
+    return data.map((item, index) => (
+      <Card key={item.UUID} style={{ width: 300, margin: 10 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={item.ImageURL}
+          alt={`Image of ${item.Name}`}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.Name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Email: {item.Email}
+            <br />
+            Info: {item.Info}
+          </Typography>
+        </CardContent>
+      </Card>
+    ));
   };
 
   return <div className="cards-grid">{createCards()}</div>;
