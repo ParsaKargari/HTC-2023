0e636125372bb491abaeccce73f15746a5c15caf
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2023-11-11 17:37:20 -0700

    just so I can make edits and integrate my branch

--- client/kids_app/src/App.js
+++ client/kids_app/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
           <Route path="/profile" element={<Profile />} />
           <Route path="/landing" element={<Landing />} />
           <Route path="/donate" element={<Donate />} />
-          <Route path="/buynow" element={<Buynow />} />
+          {/* <Route path="/buynow" element={<Buynow />} /> */}
         </Routes>
       </Router>
     </>
