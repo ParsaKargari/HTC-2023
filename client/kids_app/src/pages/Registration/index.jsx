9f1a097a91cdcfcfe9d85cde20409e8f963b3d80
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2023-11-11 17:33:00 -0700

    semi-styled registration page

--- client/kids_app/src/pages/Registration/index.jsx
+++ client/kids_app/src/pages/Registration/index.jsx
@@ -4,36 +4,66 @@ import { Navigate } from "react-router-dom";
 import user_icon from "../../assets/person.png";
 import email_icon from "../../assets/email.png";
 import password_icon from "../../assets/password.png";
+import logo from "../../assets/logo-noBackground.png";
 
 const Registration = () => {
   const [navigate, setNavigate] = useState(false);
+  const [name, setName] = useState('name');
+  const [email, setEmail] = useState('email');
+  const [password, setPassword] = useState('password');
 
   const handleNavigate = () => {
     setNavigate(true);
   };
+
   if (navigate) {
     return <Navigate to="/profile" />;
   } else {
     return (
       <div className="container">
+        <img className="logo" src={logo} alt="TinyThreads Logo" />
+        <h2 className="page-header">Login</h2>
+
         <div className="header">
           <div className="text">Sign up</div>
           <div className="underline"></div>
         </div>
-        <div className="inputs"></div>
-        <div className="input">
-          <img src={user_icon} alt="" />
-          <input type="text" name="" id="" />
+
+        <div className="inputs">
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="input">
-          <img src={email_icon} alt="" />
-          <input type="phone number" name="" id="" />
-        </div>
-        <div className="input">
-          <img src={password_icon} alt="" />
-          <input type="password" name="" id="" />
-        </div>
-        {/* <button onClick={() => <Navigate to="/profile" state={null} />}> */}
+
         <button onClick={() => handleNavigate()}>Profile</button>
       </div>
     );
