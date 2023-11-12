4b4d0a6d867f616ab6424076dec1725301ab4245
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2023-11-11 20:24:18 -0700

    integrated google auth login

--- client/kids_app/src/pages/Registration/index.jsx
+++ client/kids_app/src/pages/Registration/index.jsx
@@ -1,20 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 import { Navigate } from "react-router-dom";
 import user_icon from "../../assets/person.png";
 import email_icon from "../../assets/email.png";
 import password_icon from "../../assets/password.png";
 import logo from "../../assets/logo-noBackground.png";
+import {jwtDecode as decode} from "jwt-decode";
+
+
+const clientId = "970353632939-3d743a1i9uu5q2l0esequd3uluqgtt7p.apps.googleusercontent.com";
+
 
 const Registration = () => {
   const [navigate, setNavigate] = useState(false);
   const [name, setName] = useState('name');
   const [email, setEmail] = useState('email');
   const [password, setPassword] = useState('password');
+  const [user, setUser] = useState({});
+
+  function handleCallBacklResponse(response) {
+    // console.log("encoded jwt id token: " + response.credential.id_token);  original coded message
+    var userInfo = decode(response.credential);
+    console.log(userInfo);
+    setUser(userInfo);
+  }
 
   const handleNavigate = () => {
     setNavigate(true);
   };
+
+  useEffect(() => {
+
+    /*global google */
+    google.accounts.id.initialize({
+      client_id: clientId,
+      callback: handleCallBacklResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("SignInDiv"), 
+      {theme: "outline", size:"large"}
+    );
+  },[] );
 
   if (navigate) {
     return <Navigate to="/profile" />;
@@ -29,7 +56,21 @@ const Registration = () => {
           <div className="underline"></div>
         </div>
 
+        
+
         <div className="inputs">
+
+
+        <div id='SignInDiv'></div>
+     {user &&
+        <div>
+          <h2>{user.email}</h2>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          
+        </div>
+     }
+
           <div className="input">
             <img src={user_icon} alt="" />
             <input
