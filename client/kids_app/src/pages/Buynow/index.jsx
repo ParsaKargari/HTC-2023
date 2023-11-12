2e21c45cd6e64a03eefcfa0ee7258f92f3661de9
ParsaKargari <parsa.kargari@ucalgary.ca>
2023-11-11 21:47:02 -0700

    Add axios library and implement handleBuyNow function

--- client/kids_app/src/pages/Buynow/index.jsx
+++ client/kids_app/src/pages/Buynow/index.jsx
@@ -5,6 +5,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import axios from 'axios';
 import "./style.css";
 
 const Buynow = () => {
@@ -27,6 +28,26 @@ const Buynow = () => {
     return () => clearInterval(intervalId); // Clear interval on component unmount
   }, []);
 
+  const handleBuyNow = async (UUID, email) => {
+    try {
+      // Construct the payload
+      const payload = {
+        UUID,
+        email: email
+      };
+      
+      // Send the POST request
+      const response = await axios.post('http://10.13.164.140:8628/buy', payload);
+
+      // Handle response
+      console.log(response.data);
+      // You might want to show a success message or handle the UI change
+    } catch (error) {
+      console.error("Error during purchase:", error);
+      // Handle errors, possibly showing an error message to the user
+    }
+  };
+
   const createCards = () => {
     return data.map((item) => (
       <Card key={item.UUID} sx={{ maxWidth: 345, m: 2 }}>
@@ -48,7 +69,7 @@ const Buynow = () => {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small">Buy Now</Button>
+          <Button size="small" onClick={() => handleBuyNow(item.UUID, item.Email)}>Buy Now</Button>
         </CardActions>
       </Card>
     ));
