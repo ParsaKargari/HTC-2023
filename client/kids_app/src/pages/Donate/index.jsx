efab2e89a05ecf7bdc131b07be9c58e5995b3527
mahirazabin <mahirazabin@gmail.com>
2023-11-11 21:21:10 -0700

    prev push

--- client/kids_app/src/pages/Donate/index.jsx
+++ client/kids_app/src/pages/Donate/index.jsx
@@ -3,13 +3,13 @@ import "./style.css";
 
 const Donate = () => {
   return (
-    <form>
+    <form className="Donate-Page">
       <h1>Enter Item Details <span>Below</span></h1>
       <input type="text" title="title" id="" placeholder="Enter Title"/>
       <input type="size" title="size" id="" placeholder="Size (S/M/L)"/> 
       <textarea name="description" id="" cols="30" rows="10" placeholder="Description (Optional)"/>
-      <button type='upload'>Upload Image</button>
-      <button type='submit'>send</button>
+      <button className = "button0"type='upload'>Upload Image</button>
+      <button className = "button1"type='submit'>Submit</button>
     </form>
   )
 };
